440f37631cc70c3c36cb122e9cb61a153f1221f8
sadanand pandey <pandeysadanad75@gmail.com>
2022-05-02 23:15:00 +0530

    Added: finding maximum of 3 float number

--- src/com/bl/generics/MaximumNumber.java
+++ src/com/bl/generics/MaximumNumber.java
@@ -9,25 +9,42 @@ import java.util.Scanner;
 public class MaximumNumber {
 
 	public static void main(String[] args) {
-
-		campareTo(56, 90, 70);
+		compareTo(12.5F, 32.85F, 25.25F);
 	}
 
 	/*
 	 * comparing objects , to find maximum number
 	 */
 	public static void campareTo(int num1, int num2, int num3) {
-		Integer object1 = new Integer(num1);
-		Integer object2 = new Integer(num2);
-		Integer object3 = new Integer(num3);
-		
-		if (object1.compareTo(object2) > 0 && object1.compareTo(object3) > 0)
-			System.out.println("object1 is maxximum");
+		Integer number1 = new Integer(num1);
+		Integer number2 = new Integer(num2);
+		Integer number3 = new Integer(num3);
 
-		else if (object2.compareTo(object1) > 0 && object2.compareTo(object3) > 0)
-			System.out.println("Object2 is maximum");
+		if (number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
+			System.out.println("number1 is maximum");
+
+		else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
+			System.out.println("number2 is maximum");
 
 		else
-			System.out.println("Object3 is maximum");
+			System.out.println("number3 is maximum");
+	}
+
+	/*
+	 * comparing 3float objects , to find maximum number
+	 */
+	public static void compareTo(float num1, float num2, float num3) {
+		Float number1 = new Float(num1);
+		Float number2 = new Float(num2);
+		Float number3 = new Float(num3);
+
+		if (number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
+			System.out.println("number1 is maxximum");
+
+		else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
+			System.out.println("number2 is maximum");
+
+		else
+			System.out.println("number3 is maximum");
 	}
 }
