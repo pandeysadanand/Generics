b420060d9d49e19068947d3b5326c4746dc8048b
sadanand pandey <pandeysadanad75@gmail.com>
2022-05-03 00:23:41 +0530

    Added: Refactor 2 using constructor for all

--- src/com/bl/generics/MaximumNumber.java
+++ src/com/bl/generics/MaximumNumber.java
@@ -4,9 +4,16 @@ package com.bl.generics;
  * @author sadanand pandey
  * Finding maximum number using compareTo method
  */
-public class MaximumNumber {
+public class MaximumNumber <T extends Comparable<T>>{
+	T var1, var2, var3;
+	
+	MaximumNumber(T var1, T var2, T var3){
+		this.var1 = var1;
+		this.var2 = var2;
+		this.var3 = var3;
+	}
 	 //compareTo method for Checking Maximum from 3 Object
-    public static <T extends Comparable<T>> T compareTo(T var1,T var2, T var3) {
+	public T compareTo() {
         if(var1.compareTo(var2) > 0){
             if(var1.compareTo(var3) > 0) {
                 return var1;
@@ -29,13 +36,13 @@ public class MaximumNumber {
         System.out.println("===============================================");
         System.out.println();
         //Creating Generic Integer Object for Integer Comparison
-        MaximumNumber integerComparison = new MaximumNumber();
-        System.out.println("Maximum from 3 Integer Object : "+integerComparison.compareTo(56, 70, 60));
+        MaximumNumber<Integer> integerComparison = new MaximumNumber(56, 70, 60);
+        System.out.println("Maximum from 3 Integer Object : "+integerComparison.compareTo());
         //Creating Generic Float Object for Float Comparison
-        MaximumNumber floatComparison = new MaximumNumber();
-        System.out.println("Maximum from 3 Float Object   : "+floatComparison.compareTo(90.25F,120.63F,90.56F));
+        MaximumNumber floatComparison = new MaximumNumber(90.25F,120.63F,90.56F);
+        System.out.println("Maximum from 3 Float Object   : "+floatComparison.compareTo());
         //Creating Generic String Object for String Comparison
-        MaximumNumber stringComparison = new MaximumNumber();
-        System.out.println("Maximum from 3 String Object  : "+stringComparison.compareTo("Indai", "UK", "USA"));
+        MaximumNumber stringComparison = new MaximumNumber("Indai", "UK", "USA");
+        System.out.println("Maximum from 3 String Object  : "+stringComparison.compareTo());
     }
 }
